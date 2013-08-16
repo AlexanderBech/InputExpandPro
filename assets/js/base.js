c2de492def7efdb851c3e8af50730afbbd975d68
Alexander Bech <github@alexanderbech.com>
2013-08-16 11:20:04 +0200

    Update base.js

--- assets/js/base.js
+++ assets/js/base.js
@@ -3,4 +3,5 @@ $(document).ready(function() {
 	// $('form input').autoExpand();
 	// $('input').autoExpand();
 	// $('#id_of_input_or_textarea').autoExpand();
+	// $('input').autoExpand({ width: 20, height: 20 });
 });
